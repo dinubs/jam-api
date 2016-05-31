d2bfb7e882cea129f563e527d540c6380f728efe
Gavin Dinubilo <gavincodes@gmail.com>
2016-05-31 11:40:24 -0700

    this should fix the cors issue

--- routes.js
+++ routes.js
@@ -5,6 +5,19 @@ module.exports = function(server) {
   server.route({method: 'GET', path: '/', handler: c.Base.index});
   server.route({method: 'POST', path: '/', handler: c.Base.parse});
 
+  server.route({
+    path: '/{p*}',
+    method: 'OPTIONS',
+    handler: function(req, reply){
+      console.log('test');
+      reply({method: 'options'});
+    },
+    config: {
+      auth: false,
+      cors: true
+    }
+  });
+
   // Static files
   server.route({
     method: 'GET',
